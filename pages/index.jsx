d2c1eb6a7a5326204abc494e552aa9d51ddc2c08
knavesx <canates051@icloud.com>
2024-09-12 18:30:13 +0300

    knavesx
    knavescan

---
+++ pages/index.jsx
@@ -0,0 +1,59 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import swr from '../lib/swr';
+import Tippy from '@tippyjs/react';
+
+
+export default function Home() {
+  const { data: _repositories } = swr("/api/repos");
+  const repositories = _repositories ? _repositories : null;
+
+  return (
+    <>
+      <div className="bg-neutral-800/10 shadow-xl rounded-lg w-full h-auto mt-6">
+          <div className="relative">
+            <div className="flex flex-col lg:flex-row justify-between w-full p-6 px-8 items-center h-full">
+              <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
+                <div className="flex items-center">
+                <p className="flex items-center text-white text-4xl font-semibold">
+                  Knaves
+                </p>
+            
+                </div>
+                <p className="text-white/50 text-md mt-3">
+I am a professional editor and software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management.
+                </p>
+              </div>
+              <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
+                <img alt="knaves" src={`https://ugc.production.linktr.ee/6RJk9s2pQZ2yAdcxw3Ir_TXX2dLlNmwM2OFdf?io=true&size=avatar-v3_0`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
+              </div>
+              
+            </div>
+            <br></br>
+            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Content Creator / Manager</span>
+          </div>
+      </div>
+
+        <div className="bg-neutral-800/10 shadow-xl rounded-lg w-full h-auto mt-6">
+          <div className="relative">
+            <div className="flex flex-col lg:flex-row justify-between w-full p-6 px-8 items-center h-full">
+              <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
+                <div className="flex items-center">
+                <p className="flex items-center text-white text-4xl font-semibold">
+                  About Me
+                </p>
+            
+                </div>
+                <p className="text-white/50 text-md mt-3">
+                  I'm a 19-year-old living in London, England. I am originally from Turkey. My first project is <a href="https://discord.gg/qeW6Gjz3Zd</a>, a software publishing platform. I have long term experience in community management and general project planning. I am a professional content producer and have more than 55k followers in the field of software and cryptocurrency on social media. I am a fullstack web developer, but as of 2024, I am not actively developing a full project. 
+                </p>
+              </div>
+              
+            </div>
+          </div>
+      </div>
+
+
+    </>
+  )
+}
